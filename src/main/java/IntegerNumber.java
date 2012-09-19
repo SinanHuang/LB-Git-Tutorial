0492fbc0f6bc42180370dadab595abc994660147
Sinan Huang <sinan.huang@gmail.com>
2012-09-19 23:13:58 +0100

    Sinan: add new test for sum functionality

--- src/main/java/IntegerNumber.java
+++ src/main/java/IntegerNumber.java
@@ -1,3 +1,5 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class IntegerNumber {
     private final int value;
 
@@ -5,7 +7,15 @@ public class IntegerNumber {
         this.value = value;
     }
 
+    public int getValue(){
+        return value;
+    }
+
     public boolean isGreaterThan(IntegerNumber other) {
         return value - other.value > 0;
+    }
+
+    public IntegerNumber add(IntegerNumber other) {
+        throw new NotImplementedException(); // 亲，等你来实现
     }
 }
--- src/main/java/IntegerNumberTest.java
+++ src/main/java/IntegerNumberTest.java
@@ -1,17 +1,25 @@
-// Someone else can import JUnit :P
-//import org.junit.Test;
+import org.junit.Test;
 
-//import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class IntegerNumberTest {
+    @Test
     public void should_calculate_a_number_is_greater_than_another(){
         IntegerNumber bigNumber = new IntegerNumber(5);
-        IntegerNumber smallNumber = new IntegerNumber(10);
+        IntegerNumber smallNumber = new IntegerNumber(1);
 
-        //assertTrue(bigNumber.isGreaterThan(smallNumber));
+        assertTrue(bigNumber.isGreaterThan(smallNumber));
     }
 
-    public static void main(String[] args) {
-	    should_calculate_a_number_is_greater_than_another();
+    @Test
+    public void should_calculate_the_sum_of_two_integer_numbers(){
+        IntegerNumber aNumber = new IntegerNumber(5);
+        IntegerNumber another = new IntegerNumber(1);
+
+        IntegerNumber sum = aNumber.add(another);
+
+        int expectedResult = 6;
+        assertEquals(sum.getValue(), expectedResult);
     }
 }
